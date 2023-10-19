88c1a24d0704265c8ce99f474fded6171cfd1f02
Naiem-Devs <dnsnaiem@gmail.com>
2023-10-19 17:26:10 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+  $('.siteBar-btn').click( function (){ 
+    $('.mobile-menu').toggleClass('siteBar');   
+  }); 
+
+
+  // owlCarousel
+  $(".logo_slider").owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 6,
+    navText: [
+      '<i class="left"><img src="assets/img/left.svg" alt=""></i>',
+      '<i class="right"><img src="assets/img/right.svg" alt=""></i>'
+    ],
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2
+      },
+      767: {
+        items: 3
+      },
+      992: {
+        items: 5
+      },
+      1200: {
+        items: 6
+      }
+    }
+  });
+
+ 
+})(jQuery);
